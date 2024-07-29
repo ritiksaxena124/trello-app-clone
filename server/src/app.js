1bded5f5bca3c6a7785733437c29c31ea74e2326
ritiksaxena124 <ritiksaxena124@gmail.com>
2024-07-29 22:09:01 +0530

    registration and logout route added
    Signed-off-by: ritiksaxena124 <ritiksaxena124@gmail.com>

---
+++ server/src/app.js
@@ -0,0 +1,33 @@
+import dotenv from "dotenv";
+import express from "express";
+import authRouter from "./routers/authRouter.js";
+import cors from "cors";
+import cookieParser from "cookie-parser";
+dotenv.config();
+
+const app = express();
+const port = process.env.PORT || 9081;
+
+const options = {
+  origin: "http://localhost:3000",
+  credentials: true,
+};
+
+app.use(cors(options));
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(cookieParser());
+
+app.use("/api/v1/auth", authRouter);
+
+const serverStart = async () => {
+  try {
+    app.listen(port, (req, res) => {
+      console.log(`Server is running on http://localhost:${port}`);
+    });
+  } catch (err) {
+    console.log(err);
+  }
+};
+
+serverStart();
--- server/src/routers/authRouter.js
+++ server/src/routers/authRouter.js
@@ -1,9 +1,48 @@
-import express from "express"
+import express from "express";
+import bcrypt from "bcryptjs";
+import jwt from "jsonwebtoken";
 
 const router = express.Router();
 
-router.get("/login", (req, res) => {
-    res.send("Login")
-})
+router.post("/login", (req, res) => {
+  const { email, password } = req.body;
 
+  res.send("Login");
+});
+
+router.post("/register", (req, res) => {
+  const { fullName, email, password } = req.body;
+  bcrypt.genSalt(10, (err, salt) => {
+    if (err) {
+      console.log("Error generating salt", err);
+      throw err;
+    }
+
+    bcrypt.hash(password, salt, (err, result) => {
+      if (err) {
+        console.log("Error hashing password", err);
+        throw err;
+      }
+      // logic to create a user in database goes here
+      // create a jwt token
+      const token = jwt.sign({}, process.env.JWT_SECRET);
+
+      const options = {
+        httpOnly: true,
+        secure: true,
+      };
+      res.cookie("token", token, options);
+      res.status(201).json({
+        message: "User registered successfully!",
+        token,
+        status: 201,
+      });
+    });
+  });
+});
+
+router.get("/logout", (req, res) => {
+  res.clearCookie("token");
+  res.json({ message: "Logged out successfully" });
+});
 export default router;

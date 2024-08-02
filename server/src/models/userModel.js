10a85274111bcd4f08c258141125c8643d2fb44e
Ritik Saxena <ritiksaxena124@gmail.com>
2024-08-03 00:17:08 +0530

    added function to hash the password before saving to the DB
    Signed-off-by: Ritik Saxena <ritiksaxena124@gmail.com>

--- server/src/models/userModel.js
+++ server/src/models/userModel.js
@@ -1,4 +1,5 @@
 import mongoose from "mongoose";
+import bcrypt from "bcryptjs";
 
 const userSchema = mongoose.Schema({
   fullName: {
@@ -21,6 +22,22 @@ const userSchema = mongoose.Schema({
     },
   ],
 });
+
+
+
+// This "pre" hook runs before saving the data into the database
+userSchema.pre("save", async function (next) {
+  // Don't run the logic if the password field is not changed
+  if (!this.isModified("password")) return next();
+
+  // Hash the password
+  bcrypt.genSalt(10, function (err, salt) {
+    bcrypt.hash(this.password, salt, function (err, hash) {
+      this.password = hash;
+      next();
+    })
+  })
+})
 
 const User = mongoose.model("User", userSchema);
 

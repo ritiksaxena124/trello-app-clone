5ce54ab7d8c6460bc869baeca1502f1d316ec671
Ritik Saxena <ritiksaxena124@gmail.com>
2024-08-03 18:35:24 +0530

    login user controller rewrite
    Signed-off-by: Ritik Saxena <ritiksaxena124@gmail.com>

--- server/src/controllers/user.controller.js
+++ server/src/controllers/user.controller.js
@@ -1,8 +1,21 @@
 import User from "../models/userModel.js";
+import jwt from "jsonwebtoken";
 
 import { asyncHandler } from "../utils/asyncHandler.js";
 import { ApiError } from "../utils/ApiError.js";
 import { ApiResponse } from "../utils/ApiResponse.js";
+
+const generateAccessAndRefreshToken = async (userId) => {
+    const user = await User.findById(userId);
+
+    const accessToken = await user.generateAccessToken();
+    const refreshToken = await user.generateRefreshToken();
+
+    user.refreshToken = refreshToken;
+    await user.save();
+
+    return { accessToken, refreshToken };
+}
 
 const registerUser = asyncHandler(async (req, res) => {
     const { fullName, email, password } = req.body;
@@ -23,7 +36,7 @@ const registerUser = asyncHandler(async (req, res) => {
         password
     })
 
-    const createdUser = await User.findById(user._id).select("-password");
+    const createdUser = await User.findById(user._id).select("-password -refreshToken");
 
     // check for user creation
     if (!createdUser) {
@@ -51,9 +64,23 @@ const loginUser = asyncHandler(async (req, res) => {
         return res.status(401).json(new ApiResponse(401, "Incorrect credentials"));
     }
 
-    const user = await User.findById(userExists._id).select("-password");
+    const { accessToken, refreshToken } = await generateAccessAndRefreshToken(userExists._id);
 
-    res.status(201).json(new ApiResponse(201, "User logged in successfully", user));
+    const user = await User.findById(userExists._id).select("-password -refreshToken");
+
+    const options = {
+        httpOnly: true,
+        secure: true
+    }
+
+    return res
+        .status(201)
+        .cookie("accessToken", accessToken, options)
+        .cookie("refreshToken", refreshToken, options)
+        .json(new ApiResponse(201, "User logged in successfully", {
+            user,
+            accessToken, refreshToken
+        }));
 })
 
 const logoutUser = asyncHandler(async (req, res) => {
@@ -61,4 +88,25 @@ const logoutUser = asyncHandler(async (req, res) => {
     res.status(200).json(new ApiResponse(200, "User logged out successfully"));
 })
 
-export { registerUser, logoutUser, loginUser };
+const retrieveUserData = asyncHandler(async (req, res) => {
+    const token = req.headers.cookie?.replace("token=", "");
+    if (!token) {
+        return res.status(401).json(new ApiError(401, "Unauthorized access"))
+    }
+    const blob = jwt.verify(token, process.env.JWT_SECRET);
+
+    if (!blob) {
+        return res.status(401).json(new ApiError(401, "Unauthorized access"))
+    }
+
+    const user = await User.findById({
+        _id: blob.id,
+    }).populate("tasksData").select("-password");
+
+    if (!user) {
+        res.status(500).json(new ApiError(500, "Failed to retrieve user data"));
+    }
+    res.status(201).json(new ApiResponse(201, "User data retrieved successfully", user));
+})
+
+export { registerUser, logoutUser, loginUser, retrieveUserData };

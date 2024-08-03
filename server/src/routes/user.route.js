6e63881c853c60744a03c22664fb94da7bbb06a5
Ritik Saxena <ritiksaxena124@gmail.com>
2024-08-03 18:34:36 +0530

    cleaned user routes file
    Signed-off-by: Ritik Saxena <ritiksaxena124@gmail.com>

--- server/src/routes/user.route.js
+++ server/src/routes/user.route.js
@@ -1,34 +1,12 @@
 import express from "express";
-import User from "../models/userModel.js";
-import jwt from "jsonwebtoken";
 
-import { loginUser, logoutUser, registerUser } from "../controllers/user.controller.js";
+import { loginUser, logoutUser, registerUser, retrieveUserData } from "../controllers/user.controller.js";
 
 const router = express.Router();
 
 router.route("/login").post(loginUser);
-
 router.route("/register").post(registerUser);
-
 router.route("/logout").get(logoutUser);
-
-router.get("/getdata", async (req, res) => {
-  const token = req.headers.cookie?.replace("token=", "");
-  if (!token) {
-    return res.status(401).json({
-      msg: "Unauthorized",
-      status: 401
-    })
-  }
-  const blob = jwt.verify(token, process.env.JWT_SECRET);
-  const user = await User.findById({
-    _id: blob.id,
-  }).populate("tasksData").select("-password");
-
-  res.json({
-    user,
-    status: 201
-  });
-});
+router.route("/getdata").get(retrieveUserData);
 
 export default router;

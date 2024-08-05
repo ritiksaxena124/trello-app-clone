f8513bae87b1408568215cf7532fd05490e0485d
Ritik Saxena <ritiksaxena124@gmail.com>
2024-08-06 01:02:35 +0530

    updated task model schema
    Signed-off-by: Ritik Saxena <ritiksaxena124@gmail.com>

---
+++ server/src/models/task.model.js
@@ -0,0 +1,33 @@
+import mongoose from "mongoose";
+
+const taskSchema = mongoose.Schema(
+  {
+    title: {
+      type: String,
+      required: true,
+    },
+    description: {
+      type: String,
+    },
+    status: {
+      type: String,
+      enum: ["Todo", "In Progress", "Under Review", "Finished"],
+      required: true,
+      default: "todo",
+    },
+    priority: {
+      type: String,
+      enum: ["Urgent", "Medium", "Low"],
+    },
+    userId: {
+      type: mongoose.Schema.Types.ObjectId("User")
+    }
+  },
+  {
+    timestamps: true,
+  }
+);
+
+const Task = mongoose.model("Task", taskSchema);
+
+export { Task };

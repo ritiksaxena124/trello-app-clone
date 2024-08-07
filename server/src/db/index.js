eb2cd5d23d7dce29378e53535a158dd91ce9ab11
Ritik Saxena <ritiksaxena124@gmail.com>
2024-08-08 00:30:41 +0530

    chore: Update MongoDB connection URI to use a fallback value
    Signed-off-by: Ritik Saxena <ritiksaxena124@gmail.com>

--- server/src/db/index.js
+++ server/src/db/index.js
@@ -1,8 +1,9 @@
 import mongoose from "mongoose";
 
 const connectDB = async () => {
+  const DB_URI = "mongodb://localhost:27017/trello-app"
   try {
-    const connectionInstance = await mongoose.connect(process.env.MONGODB_URI);
+    const connectionInstance = await mongoose.connect(process.env.MONGODB_URI || DB_URI);
     console.log(`MongoDB Connected: ${connectionInstance.connection.host}`);
   } catch (err) {
     console.error("Failed to connect with DB", err);
